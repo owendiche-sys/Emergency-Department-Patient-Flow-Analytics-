7d07ae16c0582fb6380b842412d3a079e3a079d3
Adeniyi Tijesunimi <nimiperkins24@gmail.com>
2026-07-19 14:11:02 +0100

    sql: create staging and final ED tables

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -2,5 +2,5 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/nhamcs_2022_visi
 INTO TABLE staging_ed_visits
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
